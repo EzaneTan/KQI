555b9db476c2d62f878586ae290d3b91d5a78532
Ezane Tan <diablososa@proton.me>
2025-05-17 04:04:13 -0700

    Create main.tf

---
+++ architecture/terraform/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.27"
+  subnets         = var.private_subnets
+  vpc_id          = var.vpc_id
+  # …  
+}
